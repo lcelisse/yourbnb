fceb55004fecabfc31401fad6613becd70dab4e8
lilly <lcrhidalgo99@gmail.com>
2022-12-21 14:35:52 -0500

    Get Details od a Spot by Id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -81,4 +81,57 @@ router.get("/current", async (req, res) => {
   return res.json({ Spots: spotsList });
 });
 
+//Get details for a spot from an id
+
+router.get("/:spotId", async (req, res, next) => {
+  //get spot
+  //   const { spotId } = req.params;
+  const spot = await Spot.findByPk(req.params.spotId, {
+    raw: true,
+  });
+
+  if (!spot) {
+    const err = new Error("Spot couldn't be found");
+    err.status = 404;
+    return next(err);
+  }
+  // number of reviews and average of stars
+  const reviews = await Review.findAll({
+    where: {
+      spotId: spot.id,
+    },
+    attributes: ["review", "stars"],
+    raw: true,
+  });
+
+  let sum = 0;
+  let total = reviews.length;
+  for (let review of reviews) {
+    sum += review.stars;
+  }
+
+  spot.numReviews = reviews.length;
+  spot.avgStarRating = sum / total;
+
+  const spotImgs = await SpotImage.findAll({
+    where: {
+      preview: true,
+      spotId: req.params.spotId,
+    },
+    attributes: ["id", "url", "preview"],
+  });
+
+  spot.SpotImages = spotImgs;
+
+  const owner = await User.findOne({
+    where: {
+      id: spot.ownerId,
+    },
+    attributes: ["id", "firstName", "lastName"],
+  });
+
+  spot.Owner = owner;
+
+  res.json(spot);
+});
 module.exports = router;

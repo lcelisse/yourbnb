88ce9eefe247a5f1df6b24a6c9959df4e7110488
lilly <lcrhidalgo99@gmail.com>
2022-12-21 13:11:52 -0500

    Get Spots of Current User

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -46,4 +46,39 @@ router.get("/", async (req, res) => {
   return res.json({ Spots: spotsList });
 });
 
+//all spots of current owner
+
+router.get("/current", async (req, res) => {
+  const spots = await Spot.findAll({
+    where: {
+      ownerId: req.user.id,
+    },
+  });
+  const spotsList = [];
+  // get avg ratings
+  for (let spot of spots) {
+    const review = await Review.findAll({
+      where: {
+        spotId: spot.id,
+      },
+      attributes: [[Sequelize.fn("AVG", Sequelize.col("stars")), "avgRating"]],
+      raw: true,
+    });
+
+    //include preview image
+    const previewImg = await SpotImage.findOne({
+      where: { preview: true, spotId: spot.id },
+    });
+
+    let spotList = {
+      ...spot.dataValues,
+      avgRating: Number(review[0].avgRating),
+      previewImage: previewImg.url,
+    };
+    spotsList.push(spotList);
+  }
+
+  return res.json({ Spots: spotsList });
+});
+
 module.exports = router;

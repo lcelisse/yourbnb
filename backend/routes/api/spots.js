811f63278bdb6c2f94c61e7ab21df9ff616c150e
lilly <lcrhidalgo99@gmail.com>
2023-01-02 12:44:01 -0500

    remove auth from get bookings by spot id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -501,7 +501,7 @@ router.post(
 );
 
 //bookings by spot id
-router.get("/:spotId/bookings", requireAuth, async (req, res, next) => {
+router.get("/:spotId/bookings", async (req, res, next) => {
   const spot = await Spot.findByPk(req.params.spotId);
 
   if (!spot) {
@@ -566,7 +566,7 @@ router.post("/:spotId/bookings", requireAuth, async (req, res, next) => {
       },
       raw: true,
     });
- 
+
     for (let booking of bookings) {
       let { startDate, endDate } = booking;
       startDate = new Date(startDate);

262e3f5145835c8d708f77bcb467ff5a5cefb8bf
lilly <lcrhidalgo99@gmail.com>
2023-01-13 14:48:54 -0500

    fixes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,7 +7,7 @@ const spotRouter = require("./spots.js");
 const reviewsRouter = require("./reviews.js");
 const bookingRouter = require("./bookings.js");
 const spotImagerouter = require("./spot-images.js");
-const reviewImgRouter = require('./review-images')
+const reviewImgRouter = require("./review-images");
 router.use(restoreUser);
 
 // router.get("/restore-user", (req, res) => {
@@ -22,7 +22,7 @@ router.use("/spots", spotRouter);
 router.use("/reviews", reviewsRouter);
 router.use("/bookings", bookingRouter);
 router.use("/spot-images", spotImagerouter);
-router.use("/review-images", reviewImgRouter)
+router.use("/review-images", reviewImgRouter);
 // router.post("/test", (req, res) => {
 //   res.json({ requestBody: req.body });
 // });

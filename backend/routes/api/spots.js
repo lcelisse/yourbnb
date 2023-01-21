6c8ec3620a39f2996c5b30c9e8b360d663b8d243
lilly <lcrhidalgo99@gmail.com>
2023-01-21 09:15:37 -0500

    progress

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -32,12 +32,7 @@ const validateCreatedSpot = [
   check("country")
     .exists({ checkFalsy: true })
     .withMessage("Country is required"),
-  check("lat")
-    .exists({ checkFalsy: true })
-    .withMessage("Latitude is not valid"),
-  check("lng")
-    .exists({ checkFalsy: true })
-    .withMessage("Longitude is not valid"),
+
   check("name")
     .exists({ checkFalsy: true })
     .isLength({ max: 49 })
@@ -245,7 +240,7 @@ router.get("/current", requireAuth, async (req, res) => {
     let spotList = {
       ...spot.dataValues,
       avgRating: Number(review[0].avgRating),
-      previewImage: previewImg ? previewImg.url : 'none',
+      previewImage: previewImg ? previewImg.url : null,
     };
     spotsList.push(spotList);
   }
@@ -312,8 +307,7 @@ router.get("/:spotId", async (req, res, next) => {
 //Create Spot
 
 router.post("/", requireAuth, validateCreatedSpot, async (req, res) => {
-  const { address, city, state, country, lat, lng, name, description, price } =
-    req.body;
+  const { address, city, state, country, name, description, price } = req.body;
 
   const user = await User.findOne({
     where: {
@@ -327,8 +321,6 @@ router.post("/", requireAuth, validateCreatedSpot, async (req, res) => {
     city: city,
     state: state,
     country: country,
-    lat: lat,
-    lng: lng,
     name: name,
     description: description,
     price: price,
@@ -369,17 +361,8 @@ router.put(
   validateCreatedSpot,
   authorization,
   async (req, res) => {
-    const {
-      address,
-      city,
-      state,
-      country,
-      lat,
-      lng,
-      name,
-      description,
-      price,
-    } = req.body;
+    const { address, city, state, country, name, description, price } =
+      req.body;
 
     const edited = await Spot.findByPk(req.params.spotId);
     if (!edited) {
@@ -392,8 +375,6 @@ router.put(
       city,
       state,
       country,
-      lat,
-      lng,
       name,
       description,
       price,
